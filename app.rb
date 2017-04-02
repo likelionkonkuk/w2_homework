e042d1587c18d673de6369e75eb8288dd090e6e7
mongddangand <mongddangand@naver.com>
2017-04-03 00:26:10 +0900

    myungsang

--- app.rb
+++ app.rb
@@ -45,96 +45,116 @@ end
 def upcase
   shin = "critical"
   puts shin.upcase  #문자열을 대문자로 바꾸어줌.
+  #CRITICAL
 end
 
 def downcase
   shin = "CRITICAL"
   puts shin.downcase #문자열을 소문자로 바꾸어줌.
+  #critical
 end
 
 def length
   shin = "critical"
   puts shin.length #문자열의 길이를 알려준다.
+  #8
 end
 
 def reverse
   shin = "critical"
   puts shin.reverse #문자열을 뒤집어준다.
+  #lacitirc
 end
 
 def capitalize
   shin = "critical"
-  puts shin.capitalize
+  puts shin.capitalize #대문자를 만들어준다.
+  #Critical
 end
 
 def include?
   shin = "critical"
-  puts shin.include? "" #""안에 문자를 포함하는지 아닌지를 boolean값으로 알려준다.
+  puts shin.include? "c" #""안에 문자를 포함하는지 아닌지를 boolean값으로 알려준다.
+  #true
+
 end
 
 def gsub
   shin = "critical"
-  puts shin.gsub(//,"") #//안에있는 문자가 shin안에 있다면 ""문자로 바꾸어준다.
+  puts shin.gsub(/r/,"c") #//안에있는 문자가 shin안에 있다면 ""문자로 바꾸어준다.
+  #ccitical
 end
 
 def spilt
   shin = "cri tial"
   puts shin.spilt #문자를 띄어쓰기 전후로 분리하여 배열로 만들어준다.
+  #'cri'tical'
 end
 
 def select
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.select{|x|x==i} #shin에서 select조건에 맞는 것을 출력함.
+  #"i","i"
 end
 
 def reject
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.reject{|x|x==i} #shin에서 reject조건에 맞는 것은 출력하지 않음.
+  #"c","r","t","c","a","l"
 end
 
 def push
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.push["h","i","t"] #shin에 h,i,t을 추가해줌.
+  #["c","r","i","t","i","c","a","l","h","i","t"]
 end
 
 def detect
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.detect{|x|x=="i"} #shin에서 detect 조건에 맞는 것을 찾아줌.
+  #"i","i"
 end
 
 def shift
   shin = ["c","r","i","t","i","c","a","l"]
-  puts shin.shift(i) #shin에서 처음부터 i까지의 값을 삭제함.
+  puts shin.shift(r) #shin에서 처음부터 r까지의 값을 삭제함.
+  #["i","t","i","c","a","l"]
 end
 
 def unshift
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.unshift(k) #shin의 처음에 k를 추가함.
+  #["k","c","r","i","t","i","c","a","l"]
 end
 
 def shuffle
   shin = ["c","r","i","t","i","c","a","l"]
   puts shin.shuffle #shin의 순서를 랜덤하게 섞어줌.
+  #[random하게 섞임]
 end
 
 def collect
   shin = [3,6,9]
   puts shin.collect{|x|x+=2} #shin안에 있는 각각의 요소를 콜랙트의 조건에따라 편집하여준다.
+  #[5,8,11]
 end
 
 def times
   shin = "critical"
   puts 3.times "#{critical}" #shin을 x.times 의 x회 반복시킴.
+  #"critical""critical""critical"
 end
 
 def max
   shin = [1...100]
   puts shin.max #shin안의 최댓값을 찾아줌.
+  #99
 end
 
 def min
   shin = [1...100]
   puts shin.min #shin안의 최솟값을 찾아줌.
+  #2
 end
 #여기 이 두 주석 사이에 메소드를 작성하시면 됩니다~
 
